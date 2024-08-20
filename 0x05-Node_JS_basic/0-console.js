13717db99d8f2e17bebaad45065762c3ae2c1885
Mmadubugwu Kingsley <141311585+Kmmadu@users.noreply.github.com>
2024-08-20 16:25:29 +0100

    Update 0-console.js

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author Mmadubugwu Kingsley <https://github.com/Kmmadu>
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
